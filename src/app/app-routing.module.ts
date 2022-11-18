623f722b70624d488ce70460c19694c114be808b
NiikoViiana <niikov11@gmail.com>
2022-11-17 20:07:42 -0500

    Carga inicial de una maqueta hecha en Angular
    Carga inicial de una maqueta hecha en Angular

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const routes: Routes = [
+  {
+    path: '', loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule)
+  }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
