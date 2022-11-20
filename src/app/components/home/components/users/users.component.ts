e3aa7fc14dffec659a9cf1ff2845a46a7e1d8785
NiikoViiana <niikov11@gmail.com>
2022-11-20 00:11:06 -0500

    Documentación
    Se realiza la documentación del codigo que realiza la logica del modulo de usuarios, incluyendo los metodos , las varables y los servicios

--- src/app/components/home/components/users/users.component.ts
+++ src/app/components/home/components/users/users.component.ts
@@ -1,5 +1,4 @@
-import { DecimalPipe } from '@angular/common';
-import { Component, OnInit, PipeTransform, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { async, map, Observable, startWith } from 'rxjs';
@@ -14,7 +13,6 @@ import { UsersService } from 'src/app/components/home/services/users.service';
 export class UsersComponent implements OnInit {
 
   @ViewChild('modal') modal: NgbActiveModal | undefined;
-
 
   listaUsuarios: Array<User> = [];  
   totalUsuarios: number = 0;
@@ -45,8 +43,7 @@ export class UsersComponent implements OnInit {
   mensaje: string = "";
 
   constructor(
-    private UsersService: UsersService, 
-    private pipe: DecimalPipe,
+    private UsersService: UsersService,
     private modalService: NgbModal,
     private fb: FormBuilder
   ) {
@@ -55,19 +52,46 @@ export class UsersComponent implements OnInit {
 
   ngOnInit(): void {    
   }
-
-  async getData() {
+  
+  /**
+   * Método asíncrono que permite la consulta de los usuarios iniciales
+   * asi como los datos filtrados para el 'observer' de usuarios, al momento de realizar el filtro de búsqueda en la tabla.
+   * Mientras se realiza la carga, se activa la variable de 'loading' para ejecutar el 'spinner' mientras se consultan los datos.
+   *
+   * @property {boolean} UsersComponent.loading - Indicador para activar o desactivar 
+   * el 'spinner' en la vista
+   * @property {Array<User>} UsersComponent.listaUsuarios - Lista de los usuarios 
+   * que se encuentran registrados
+   * @property {Observable<User[]>} UsersComponent.usuarios - Observable que contiene 
+   * los usuarios, se activa cuando existe un cambio en el filtro de la tabla
+   * @property {FormControl<string>} UsersComponent.filter - Contiene el texto del input buscar 
+   * de la tabla
+   */
+  async getData(): Promise<void> {
     this.loading        =   true;
     this.listaUsuarios  =   await this.getUsers() as Array<User>;
     
     this.usuarios$ = this.filter.valueChanges.pipe(
 			startWith(''),
-			map((text) => this.searchInTable(text, this.pipe)),
+			map((text) => this.searchInTable(text)),
 		);
 
     this.loading        =   false;
   }
 
+  /**
+   * Promesa que realiza la consulta del servicio de {@link UsersService | users.service.ts}
+   * para obtener todos los usuarios dependiendo de la pagina seleccionada
+   * Este método consulta los usuarios y los retorna al método asíncrono {@link getData() | async getData()}
+   * 
+   * En caso de no consultar los datos, imprime el error en consola
+   * 
+   * @property {number} UsersComponent.page - Numero de la pagina en la que se encuentre la tabla
+   * @property {number} UsersComponent.limit - Limite de usuarios que se visualizan en la tabla
+   * @property {number} UsersComponent.totalUsuarios - Cantidad total de usuarios registrados en la BD
+   * 
+   * @returns {Promise<Array<User>>} Lista de los usuarios que se encuentran registrados en la BD
+   */
   getUsers(): Promise<Array<User>> {
     return new Promise(resolve => {
 
@@ -83,8 +107,15 @@ export class UsersComponent implements OnInit {
 
     });
   }
-
-  searchInTable(text: string, pipe: PipeTransform): User[] {    
+  
+  /**
+   * Método para realizar el filtro de los usuarios, dependiendo de lo que se escriba en el buscador de la tabla.
+   * Se usa el método 'filter' del array de usuarios dependiendo del texto que se le pase en los parámetros 
+   * 
+   * @param {string} text - Texto que se escribe en el buscador de la tabla
+   * @returns {Array<User>} Lista de usuarios filtrado por el texto dado
+   */
+  searchInTable(text: string): User[] {    
     return this.listaUsuarios.filter((usuario) => {
       const term = text.toLowerCase();
       return (
@@ -96,12 +127,46 @@ export class UsersComponent implements OnInit {
     });
   }
 
-  refreshTable() {
+  /**
+   * Método que usa el paginador para refrescar los datos de la tabla, dependiendo de la pagina seleccionada.
+   * Limpia el buscado de la tabla y redirige a la función de {@link getData() | async getData()} para consultar
+   * los nuevos datos de la tabla.
+   * 
+   * @property {FormControl<string>} UsersComponent.filter - Contiene el texto del input buscar de la tabla
+   */
+  refreshTable(): void {
     this.filter.setValue('');
     this.getData();
 	}
 
-  async openModalUser(content: any, option: number, idUser: User['id']) {
+  /**
+   * Método asíncrono que abre el modal de usuarios.
+   * 
+   * Este modal se usa para la creación y edición de usuarios.
+   * Si es para la creación de usuarios, se limpia el formulario dejando los campos vacíos.
+   * Si es para la edición de usuarios, se consultan los datos del usuario y
+   * se insertan en el formulario.
+   * 
+   * @property {boolean} UsersComponent.loading - Indicador para activar o desactivar 
+   * el 'spinner' en la vista.
+   * 
+   * @property {boolean} UsersComponent.selectOption - Indicador para saber si es 
+   * (1)Creación de usuario o (3)Edición de usuario.
+   * 
+   * @property {NgbModal} UsersComponent.modalService - Servicio que permite hacer las
+   * interacciones con el modal, como lo es la de abrir dependiendo de la referencia que
+   * se le envié.
+   * 
+   * @property {boolean} UsersComponent.submitted - Indicador para saber si los 
+   * datos del formulario fueron o no enviados, con el fin de mostrar los mensajes de validaciones.
+   * 
+   * 
+   * @param {any} content - Referencia del modal que se quiere abrir.
+   * @param {number} option - Indicador para saber si se realizo la acción de crear o editar.
+   * @param {User.id} idUser - Id del usuario que se desea consultar si se desea 
+   * editar el usuario.
+   */
+  async openModalUser(content: any, option: number, idUser: User['id']): Promise<void> {
     
     this.loading  = true;
     this.selectOption = option;
@@ -142,7 +207,19 @@ export class UsersComponent implements OnInit {
     this.loading  = false;
 	}  
 
-  getUserById(idUser: string): Promise<User> {
+  /**
+   * Promesa que realiza la consulta del servicio de {@link UsersService.getUsersById() | getUsersById()}
+   * para obtener el usuario dependiendo del id enviado
+   * 
+   * Este método consulta el usuario y los retorna al método asíncrono {@link openModalUser() | async openModalUser()}
+   * 
+   * 
+   * En caso de no consultar los datos, imprime el error en consola
+   * 
+   * @param {User.id} idUser - Id del usuario que se desea consultar
+   * @returns {Promise<User>} - Datos del usuario registrado en la BD
+   */
+  getUserById(idUser: User['id']): Promise<User> {
     return new Promise(resolve => {
       this.UsersService.getUsersById(idUser).subscribe({
         next: (data: User) => {
@@ -153,7 +230,28 @@ export class UsersComponent implements OnInit {
     });
   }
 
-  async onSubmit() {
+  /**
+   * Método asíncrono que permite guardar los datos del formulario.
+   * 
+   * Si el id del usuario viene vació, se asume que es una creación, si viene con un dato, es
+   * para una actualización.
+   * En ambos casos, se envia un mensaje indicando la inserción o actualización de los datos.
+   * 
+   * @property {boolean} UsersComponent.submitted - Indicador para saber si los 
+   * datos del formulario fueron o no enviados, con el fin de mostrar los mensajes 
+   * de validaciones.
+   * 
+   * @property {boolean} UsersComponent.loading - Indicador para activar o desactivar 
+   * el 'spinner' en la vista.
+   * 
+   * @property {FormGroup<any>} UsersComponent.formGroupUser - FormGroup que contiene
+   * todos los datos del formulario.
+   * 
+   * Para la fecha de nacimiento, el datepicker envía los datos como un objeto que contiene
+   * año, mes y dia, por lo tanto es necesario castearlo al tipo Date para poderlo enviar
+   * al servicio de guardado.
+   */
+  async onSubmit(): Promise<void> {
 
     this.submitted  =   true;
     this.loading    =   true;
@@ -225,7 +323,18 @@ export class UsersComponent implements OnInit {
     
   }
 
-  async confirmDeleteUser(idUser: User['id']) {
+  /**
+   * Método asíncrono que se ejecuta cuando se confirma la eliminación de un usuario.
+   * 
+   * Una vez realizada la eliminación se envía el mensaje exitoso. EN caso de fallar,
+   * se notifica y se imprime el error en consola.
+   * 
+   * @property {boolean} UsersComponent.loading - Indicador para activar o desactivar 
+   * el 'spinner' en la vista.
+   * 
+   * @param {User.id} idUser - Id del usuario que se desea eliminar
+   */
+  async confirmDeleteUser(idUser: User['id']): Promise<void> {
 
     this.loading = true;
     
@@ -247,6 +356,19 @@ export class UsersComponent implements OnInit {
     );
   }
 
+  /**
+   * Promesa que realiza la inserción en el servicio de {@link UsersService.createUser() | createUser()}
+   * dependiendo de los datos enviados en el formulario.
+   * 
+   * Este método inserta el usuario y los retorna los datos 
+   * al método asíncrono {@link onSubmit() | async onSubmit()}
+   * 
+   * 
+   * En caso de no insertar los datos, retorna el error de inserción
+   * 
+   * @param {User} dataUser - Datos del usuario registrados en el formulario
+   * @returns {Promise<User>} - Datos del usuario registrado en la BD
+   */
   createUser(dataUser: User): Promise<User> {
     return new Promise((resolve, reject) => {
 
@@ -260,6 +382,19 @@ export class UsersComponent implements OnInit {
     });
   }
 
+  /**
+   * Promesa que realiza la edición en el servicio de {@link UsersService.editUser() | editUser()}
+   * dependiendo de los datos enviados en el formulario.
+   * 
+   * Este método actualiza el usuario y los retorna los datos 
+   * al método asíncrono {@link onSubmit() | async onSubmit()}
+   * 
+   * 
+   * En caso de no actualizar los datos, retorna el error de edición
+   * 
+   * @param {User} dataUser - Datos del usuario registrados en el formulario
+   * @returns {Promise<User>} - Datos del usuario registrado en la BD
+   */
   editUser(dataUser: User): Promise<User> {
     return new Promise((resolve, reject) => {
 
@@ -273,6 +408,19 @@ export class UsersComponent implements OnInit {
     });
   }
 
+  /**
+   * Promesa que realiza la eliminación en el servicio de {@link UsersService.deleteUser() | deleteUser()}
+   * dependiendo del id del usuario enviado.
+   * 
+   * Este método elimina el usuario y los retorna la datos 
+   * al método asíncrono {@link confirmDeleteUser() | async confirmDeleteUser()}
+   * 
+   * 
+   * En caso de no eliminar los datos, retorna el error de eliminación
+   * 
+   * @param {User.id} idUser - Id del usuario que se desea eliminar
+   * @returns {Promise<User.id>} - Datos del usuario eliminado en la BD
+   */
   deleteUser(idUser: User['id']): Promise<User['id']> {
     return new Promise((resolve, reject) => {
 
@@ -286,6 +434,15 @@ export class UsersComponent implements OnInit {
     });
   }
 
+  /**
+   * Método que permite establecer los datos del usuario en el FormGroup del formulario
+   * de creación y edición.
+   * 
+   * Este método se usa en el método asíncrono 
+   * {@link openModalUser() | async openModalUser()}
+   * 
+   * @param {User} dataUser - Datos del usuario consultado. 
+   */
   setDataUser(dataUser: User): void {
 
     this.formGroupUser.get('firstName')?.setValue(dataUser.firstName);
@@ -303,6 +460,12 @@ export class UsersComponent implements OnInit {
     this.formGroupUser.get('telefono')?.setValue(dataUser.phone);
   }
 
+  /**
+   * Getter que permite retornar los métodos de control del FormGroup
+   * para realizar las validaciones en la vista del formulario.
+   * 
+   * @return {FormGroup.controls} Métodos del FormGroup
+   */
   get fControls() { return this.formGroupUser.controls;  }
 
 }
--- src/app/components/home/services/users.service.ts
+++ src/app/components/home/services/users.service.ts
@@ -13,6 +13,14 @@ export class UsersService {
   
   constructor(private http: HttpClient) { }
 
+  /**
+   * Método GET que consume la API para consultar todos los usuarios dependiendo
+   * de la página actual.
+   * 
+   * @param {number} page - Pagina que se desea consultar
+   * @param {number} limit - Limite de datos que se quieren consultar.
+   * @returns {any} Datos de los usuarios, y el conteo total de usuarios.
+   */
   getUsers(page: Number, limit: Number): Observable<any> {
     return  this.http.get(environment.apiUrl + `/data/v1/user/?page=${page}&limit=${limit}`)
             .pipe(
@@ -22,6 +30,13 @@ export class UsersService {
             );
   }
 
+  /**
+   * Método GET que consume la API para consultar los datos del usuario dependiendo
+   * de su id.
+   * 
+   * @param {number} idUser - Id del usuario
+   * @returns {User} Datos del usuario consultado 
+   */
   getUsersById(idUser: User['id']): Observable<User> {
     return  this.http.get(environment.apiUrl + `/data/v1/user/${idUser}`)
             .pipe(
@@ -32,6 +47,12 @@ export class UsersService {
             );
   }  
   
+  /**
+   * Método POST que consume la API para insertar los datos de un nuevo usuario.
+   * 
+   * @param {User} user - Datos del usuario nuevo 
+   * @returns {User} Datos del usuario insertado
+   */
   createUser(user: User): Observable<User> {
     return  this.http.post(environment.apiUrl + `/data/v1/user/create`, user)
             .pipe(
@@ -42,6 +63,12 @@ export class UsersService {
             );
   }  
   
+  /**
+   * Método PUT que consume la API para editar los datos de un usuario registrado.
+   * 
+   * @param {User} user - Datos del usuario nuevo 
+   * @returns {User} Datos del usuario insertado
+   */
   editUser(user: User): Observable<User> {
     return  this.http.put(environment.apiUrl + `/data/v1/user/${user.id}`, user)
             .pipe(
@@ -52,6 +79,12 @@ export class UsersService {
             );
   } 
   
+  /**
+   * Método DEL que consume la API para eliminar el usuario dependiendo de su id.
+   * 
+   * @param {number} idUser - Id del usuario
+   * @returns {User.id} Id del usuario eliminado
+   */
   deleteUser(idUser: User['id']): Observable<User['id']> {
     return  this.http.delete(environment.apiUrl + `/data/v1/user/${idUser}`)
             .pipe(
